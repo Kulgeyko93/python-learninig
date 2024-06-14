42bde7a0638358a2bd4fa83ca71a924ae2158013
kulgeyko93 <Kulgeyko93@mail.ru>
2024-06-14 10:29:12 +0300

    small task

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-print('hi')
+flat_number = 4
+entrances = 5
+count_flats_in_entrance = 20
+count_flats_in_level = 4
+
+flat_entrance = flat_number // count_flats_in_entrance + 1
+flat_level = (flat_number % count_flats_in_entrance) // count_flats_in_level
+
+print('flat_entrance = ', flat_entrance,  'flat_level = ', flat_level) 
