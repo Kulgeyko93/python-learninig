8d46866fef74da889e3978620dc08456e17010cc
kulgeyko93 <Kulgeyko93@mail.ru>
2024-06-14 10:31:28 +0300

    feat: first func

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-flat_number = 4
-entrances = 5
-count_flats_in_entrance = 20
-count_flats_in_level = 4
+def my_print(name):
+  print(name)
 
-flat_entrance = flat_number // count_flats_in_entrance + 1
-flat_level = (flat_number % count_flats_in_entrance) // count_flats_in_level
-
-print('flat_entrance = ', flat_entrance,  'flat_level = ', flat_level) 
+my_print('adada')
